8d2d611dd67d6fa5967b0d5c3455d8b5bc522932
nilesh060698 <30437778+nilesh060698@users.noreply.github.com>
2019-05-18 01:23:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose').set('debug', true);;
 
-mongoose.connect('mongodb+srv://nilesh:123@cluster0-6bxip.mongodb.net/test?retryWrites=true',{useNewUrlParser:true},(err)=>{
+mongoose.connect('mongodb+srv://nilesh:123@cluster0-6bxip.mongodb.net/flickzeee?retryWrites=true',{useNewUrlParser:true},(err)=>{
   if(!err)
   {
     console.log("mongo db connected");
